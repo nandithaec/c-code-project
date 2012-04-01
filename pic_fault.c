2b6af231107bf35f15670adcd42d3ad364fa630b
nanditha.ec <nanditha.ec@gmail.com>
2012-04-01 07:16:01 +0000

    State machine reset and initialisations moved to functions

--- pic_fault.c
+++ pic_fault.c
@@ -20,17 +20,17 @@ int main()
         int instruction=0, opcode = 0;
 
         int reg_index=0,  reg_file_addr = 0, d=0;
-        int n=0, i=0, c=0;
+        int  i=0, c=0;
         int decode_bits=0;
         int bit=0, immediate_value=0;
-        int program_memory[PROGRAM_MEM_SIZE] ={0};
+        int program_memory[PROGRAM_MEM_SIZE] ={0};//Fill the unused array elements wih NOP
 //      int program[PROGRAM_MEM_SIZE] ={0};
         char instr_mnemonic[MNEMONIC_SIZE];
         //int PCL = 0, PC = 0, PCLATH=0;
         int loop=0;
-        char line[FILE_CHARS];  
-        int instr_from_file=0;
-        int starting_PC_value = 0;
+     //   char line[FILE_CHARS];  
+       // int instr_from_file=0;
+     //   int starting_PC_value = 0;
        
         int repeat_program_execution=0;
        // int initial_PCL=0, initial_PCLATH=0;
@@ -39,7 +39,7 @@ int main()
 		
 
 
-fnew = fopen( "output_pic_results.txt", "w" );
+fnew = fopen( "output_pic_results_1e-2.txt", "w" );
 if( fnew != NULL )
    fprintf( fnew, "Hello\n" );
 
@@ -55,130 +55,14 @@ if( fnew != NULL )
 
 
 		struct crash_parameters crash_param;
-		//Initialisation	
-		crash_param.random_number=0;
-		crash_param.reg_count=0;
-		crash_param.mem_count=0;
-		crash_param.instr_cycles=0;
-		crash_param.instr_cycles_for_error=0;
-		crash_param.crash=0;
-		crash_param.program_runs=0;
-		crash_param.crash_dueto_illegal_mem=0;
-		crash_param.crash_dueto_PC=0;
-		crash_param.crash_dueto_illegal_opcode=0;
-		crash_param.error=0;
-		crash_param.control_flow_change=0;
-		crash_param.incorrect_data=0;
-		crash_param.same_reg=0;
-		crash_param.same_PC=0;
-		crash_param. reg_index_match=0;
-		crash_param.set_no_same_PC=1; // for the very first PC
-		//clear all locations
-			for(i=0;i<NUM_OF_BITFLIPS;++i)
-                crash_param.store_PC_same[i]=0;
-			
-			for(i=0;i<MAX_CRASHES;++i)
-                crash_param.crash_at_instr[i]=0;
-			for(i=0;i<MAX_CRASHES;++i)
-                crash_param.crash_time_array[i]=0;
-			for(i=0;i<FLOW_CHANGE_MAX;++i)
-                crash_param.error_at_instr[i]=0;
-
-			for(i=0;i<NUM_OF_BITFLIPS;++i)
-                crash_param.random_reg[i]=0;
-
-			for(i=0;i<NUM_OF_BITFLIPS;++i)
-                crash_param.random_mem[i]=0;
-
-			for(i=0;i<NUM_OF_BITFLIPS;++i)
-                crash_param.store_same_reg_modification[i]=0;
-	
-	
-
+		//Initialisation of crash parameters
+		 initialise_crash_param(&crash_param);
+		
 
 //-------------------------------Initialising registers------------------------------------
-//Max register content= 255 (dec) or FF (hex)
-        pic_registers.configuration_word[11]= 1; //WDT Enabler bit
+				initialise_regs(&pic_registers);
 
-//clear all registers
-			for(i=0;i<REG_MAX;++i)
-                pic_registers.GP_Reg[i]=0; //clear all registers in register file map
-       
-        //INTCON Register
-                pic_registers.GP_Reg[0x0B]=0; //INTCON register at address 0B in the register file map
-                pic_registers.GP_Reg[0x8B]= pic_registers.GP_Reg[0x0B];
-
-        //for(i=0;i<MEM_WIDTH;++i)
-        //      pic_registers.PC=0;
-        //STACK
-                pic_registers.stack_pointer = 0;
-                pic_registers.stack[1] = 0x62;
-                pic_registers.stack[0] = 0x88;
-
-        // Status register = GP_Reg[3]
-        //Assigning some value to the carry in status reg
-//              pic_registers.GP_Reg[3]= pic_registers.GP_Reg[3] & 0xFE; //carry = 0
-                //pic_registers.GP_Reg[3]= pic_registers.GP_Reg[3] | 0x01; //carry = 1
-                pic_registers.GP_Reg[3]= 0x00;
-                pic_registers.GP_Reg[0x83]= pic_registers.GP_Reg[3]; //Bank 1 and Bank 0
-
-                pic_registers.W = 0x00;
-               
-        //TMRO
-                pic_registers.GP_Reg[1]= 0x00;
-
-        //OPTION REG
-                pic_registers.GP_Reg[0x81]= 0x00;
-
-                //FSR
-                pic_registers.GP_Reg[4]= 0x00;
-                pic_registers.GP_Reg[0x84]= pic_registers.GP_Reg[4];
-
-        //PORTA
-                pic_registers.GP_Reg[5]= 0x00;
-
-        //PORTB
-                pic_registers.GP_Reg[6]= 0x00;
-
-        //TRISA
-                pic_registers.GP_Reg[0x85]= 0x00;
-
-        //TRISB
-                pic_registers.GP_Reg[0x86]= 0x00;
-
-        //EEDATA
-                pic_registers.GP_Reg[8]= 0x00;
-
-        //EEADR
-                pic_registers.GP_Reg[9]= 0x00;
-
-        //EECON1
-                pic_registers.GP_Reg[0x88]= 0x00;
-
-        //EECON2 - not a physical register
-        //      pic_registers.GP_Reg[0x89]= 0x00;
-
-//Initialise PC values
-				 pic_registers.initial_PCL =0;
-				  pic_registers.initial_PCLATH=0;
-
-//use this address 0x02 to write into PCL:
-//PCL= GP_Reg[2] and GP_Reg[0x82]
-                pic_registers.GP_Reg[2]= 0x00;
-                pic_registers.GP_Reg[0x82]= pic_registers.GP_Reg[2]; //Bank 1 and Bank 0
-                pic_registers.PCL= pic_registers.GP_Reg[2];
-
-        //PCLATH.. use this address to write into PCLATH
-                pic_registers.GP_Reg[0x0A]= 0x00;
-                pic_registers.GP_Reg[0x8A]= pic_registers.GP_Reg[0x0A]; //Bank 1 and Bank 0
-                pic_registers.PCLATH= pic_registers.GP_Reg[0x0A];
-
-                pic_registers.PC = (pic_registers.PCL | (pic_registers.PCLATH << 8)) & 0x1FFF; //Limit to 13 bits. Program counter is 13 bits
-
-                PRINT("-----------------------------------------------------------------\n");
-                PRINT("Initial values (hex): PCL=%x, PCLATH=%x, PC(testing) = %x \n",pic_registers.PCL, pic_registers.PCLATH, pic_registers.PC);
-
-//Read in the program counter initial value
+//Read in the program counter initial value from user
 
                 printf("Enter starting PCL value (in hex): \n");
 
@@ -200,34 +84,9 @@ if( fnew != NULL )
         PRINT("New values as read from the user(hex): PCL=%x, PCLATH=%x, PC(testing) = %x \n",pic_registers.GP_Reg[2], pic_registers.PCLATH, pic_registers.PC);
 
 
-//-----------Reading instruction from file---------------------------
-        n= pic_registers.PC; // new value of n is initialised to the PC value that is read from the disassembly listing
-        starting_PC_value = pic_registers.PC;
-        PRINT("Starting PC value=%x\n",starting_PC_value);
-
-        FILE *fp;
-        fp= fopen("simple_add_assembly.c","rt");
-        if( fp == NULL )
-         {
-               puts ( "cannot open file" ) ;
-          //     exit(0) ;
-         }      
-
-          while(fgets(line, FILE_CHARS, fp) != NULL)
-           {
-                 // get a line, up to 80 chars from fp.  done if NULL 
-                 sscanf (line, "%x", &instr_from_file);
-                program_memory[n]= instr_from_file;
-                n++;
-               
-           }
-         
-                for(i=starting_PC_value;i<n;i++)
-                        printf("Instructions read from file= program_memory[%x]= %x\n",i, program_memory[i]);
-						fprintf(fnew,"Instructions read from file= program_memory[%x]= %x\n",i, program_memory[i]);
-   fclose(fp);  /* close the file prior to exiting the routine */
-
-
+//*******************Read instructions********************
+        FILE *fp;               
+		read_instr_from_file(fp,program_memory,&pic_registers,fnew);
 //-------------------------------------------------------------------------------------------
 
         printf("Status register contents:(hex) at the beginning of all operations: ");
@@ -237,19 +96,19 @@ if( fnew != NULL )
         printf("\n");
 		fprintf(fnew,"\n");
 
-		printf("Probability of a bit flip is set to: %g\n",(500.0/PROBABILITY_INVERSE));
-		fprintf(fnew,"Probability of a bit flip is set to: %g\n",(500.0/PROBABILITY_INVERSE));
+		printf("Probability of a bit flip is set to: %g\n",((float)(RANDOM_GUESS_RANGE-1)/PROBABILITY_INVERSE)); //%g gives in terms of e.g.,1e-2
+		fprintf(fnew,"Probability of a bit flip is set to: %g\n",((float)(RANDOM_GUESS_RANGE-1)/PROBABILITY_INVERSE));
 		printf("Executing...\n");
 		fprintf(fnew,"Executing...\n");
 
-        loop= starting_PC_value;
+        loop= pic_registers.starting_PC_value;
 
 //Repeat the same program forever till Ctrl-C is entered
-	while (loop < n)
+	while (loop < pic_registers.n)
         {
                
                 PRINT("****************************************************************\n");
-                PRINT("INSTRUCTION NUMBER %d\n", loop - starting_PC_value + 1);
+                PRINT("INSTRUCTION NUMBER %d\n", loop - (pic_registers.starting_PC_value) + 1);
                 PRINT("Entering execution loop with repeat = %d\n", repeat_program_execution);
 		
                 
@@ -302,7 +161,7 @@ if( fnew != NULL )
                 post_decode= pre_decode;
                       				
 				  //Bit flip function called every cycle
-	    		bit_flips(&pic_registers, program_memory, &crash_param, start_seconds, &post_decode,fnew);
+	    		bit_flips(&pic_registers, program_memory, &crash_param, start_seconds, &post_decode,fnew,fp);
 
 				if( (decode_bits ==0 || decode_bits ==1) && (pre_decode.instr_mnemonic_enum != NOP) && (pre_decode.instr_mnemonic_enum != CLRW))
 				//Check reg file access error only for byte abd bit oriented instructions and make sure it is not a NOP or CLRW
@@ -335,22 +194,11 @@ if( fnew != NULL )
 				
 
                 //Repeat program
-                if (loop == n) //If end of program is reached
+                if (loop == pic_registers.n) //If end of program is reached
                 {
-                    loop= starting_PC_value; //Reset loop to beginning of program and begin execution again
+                    loop= pic_registers.starting_PC_value; //Reset loop to beginning of program and begin execution again
 
-                    //----------------------------------------------------------------------------------------------------------------------------
-                    //Reset program counter to beginning of the program
-                    pic_registers.GP_Reg[2]= pic_registers.initial_PCL;
-                    pic_registers.GP_Reg[0x82]= pic_registers.GP_Reg[2]; //PCL Bank 1 and Bank 0
-                    pic_registers.PCL= pic_registers.GP_Reg[2];
-
-                    pic_registers.GP_Reg[0x0A]= pic_registers.initial_PCLATH;
-                    pic_registers.GP_Reg[0x8A]= pic_registers.GP_Reg[0x0A]; //PCLATH Bank 1 and Bank 0
-                    pic_registers.PCLATH= pic_registers.GP_Reg[0x0A];
-
-                    pic_registers.PC = (pic_registers.PCL | (pic_registers.PCLATH << 8)) & 0x1FFF; //Limit to 13 bits. PC= 13 bits
-                    //----------------------------------------------------------------------------------------------------------------------------
+                    reset_PC_to_beginninng(&pic_registers);
 
                     repeat_program_execution++; //Keep track of the number of times the program is re-executed
                               
@@ -362,8 +210,8 @@ if( fnew != NULL )
 
         }
 
-                printf("\nTotal number of instructions in the program = %d\n",n);   
-				fprintf(fnew,"\nTotal number of instructions in the program = %d\n",n);         
+                printf("\nTotal number of instructions in the program = %d\n",pic_registers.n);   
+				fprintf(fnew,"\nTotal number of instructions in the program = %d\n",pic_registers.n);         
                
 				 printf("Each instruction takes 1 instruction cycles, i.e., 1 clock cycle\n");
 				fprintf(fnew,"Each instruction takes 1 instruction cycles, i.e., 1 clock cycle\n");
@@ -433,10 +281,10 @@ for(c=0; c< (crash_param.error); c++)
 }
 
    
-percentage_crash= ((crash_param.crash/total_instr_cycles))*100.0;
-percentage_error= ((crash_param.error/total_instr_cycles))*100.0;
+percentage_crash= ((double)crash_param.crash/total_instr_cycles)*100.0;
+percentage_error= ((double)crash_param.error/total_instr_cycles)*100.0;
 successful_cycles= (total_instr_cycles-sum_of_all_errors- MAX_CRASHES);
-percentage_success= ((successful_cycles/total_instr_cycles))*100.0;
+
 
 printf("Total number of instruction cycles executed:%llu\n",(total_instr_cycles)); //%e
 fprintf(fnew,"Total number of instruction cycles executed:%llu\n",(total_instr_cycles)); 
@@ -454,10 +302,6 @@ fprintf(fnew,"Summing up all errors which repeat every program run,Total number 
 printf("Total number of successful instruction cycle executions without errors/crashes:%llu\n\n",(successful_cycles)); //%e
 fprintf(fnew,"Total number of successful instruction cycle executions without errors/crashes:%llu\n\n",(successful_cycles)); 
 
-printf("Percentage of successful executions:%f\n\n", ((double) successful_cycles/total_instr_cycles)*100.0); //%e
-fprintf(fnew,"Percentage of successful executions:%f\n\n", ((double)successful_cycles/total_instr_cycles)*100.0); 
-
-
 printf("Number of crashes due to Program counter getting manipulated: %d \n",crash_param.crash_dueto_PC);
 fprintf(fnew,"Number of crashes due to Program counter getting manipulated: %d \n",crash_param.crash_dueto_PC);
 
@@ -473,17 +317,18 @@ fprintf(fnew,"Total number of errors due to incorrect control flow: %d\n",crash_
 printf("Total number of errors due to incorrect data: %d\n",crash_param.incorrect_data);
 fprintf(fnew,"Total number of errors due to incorrect data: %d\n",crash_param.incorrect_data);
 
+printf("Percentage of successful executions:%f\n\n", ((double) successful_cycles/total_instr_cycles)*100.0); //%e
+fprintf(fnew,"Percentage of successful executions:%f\n\n", ((double)successful_cycles/total_instr_cycles)*100.0); 
 
 
-/*printf("\n");
-printf("Percentage of crashes due to Program counter getting manipulated: %d percent\n",(((crash_param.crash_dueto_PC)/MAX_CRASHES)*100));
-printf("Percentage of crashes due to illegal memory access: %d percent\n",(((crash_param.crash_dueto_illegal_mem)/MAX_CRASHES)*100));
-printf("Percentage of crashes due to illegal memory access: %d percent\n",(((crash_param.crash_dueto_illegal_opcode)/MAX_CRASHES)*100));
+printf("Percentage of crashes due to Program counter getting manipulated:%f percent\n",(((double)crash_param.crash_dueto_PC/MAX_CRASHES)*100.0));
+printf("Percentage of crashes due to illegal memory access:%f percent\n",(((double) crash_param.crash_dueto_illegal_mem/MAX_CRASHES)*100.0));
+printf("Percentage of crashes due to illegal memory access:%f percent\n",(((double) crash_param.crash_dueto_illegal_opcode/MAX_CRASHES)*100.0));
 
 printf("\n");
 printf("Percentage of crashes out of total number of instruction cycles: %f percent\n",percentage_crash);
 printf("Percentage of errors out of total number of instruction cycles: %f percent\n",percentage_error);
-printf("Percentage of successful cycles out of total number of instruction cycles: %f percent\n",percentage_success);*/
+
 
 mean_instr_cycles= total_instr_cycles/MAX_CRASHES;
 printf("Mean time to failure in terms of the number of instruction cycles: %llu\n", mean_instr_cycles);
@@ -501,5 +346,6 @@ return 0;
 
 
 }
+
 
 
